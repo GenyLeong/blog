4b23b88d37ea2ac06e8b777cb77303a2ed59c5d2
Génesis León <genesis.leon.carbajal@gmail.com>
2022-12-01 21:40:29 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-53a1bae7a4f4b4a7ca05.js"
+    "url": "webpack-runtime-b4a07e5995eaa4dc97d5.js"
   },
   {
     "url": "framework-fd81c2a3ecc620ec49ad.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "eb0308375f32943c113cb23f2509a910"
+    "revision": "088fba08d73d26427567287f6653e1df"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-46b4173e1060a5c82475.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1ce61276cc1cfc556d144eb7281d21b5"
+    "revision": "423ebb31a0e7574a7850006d69425c4b"
   },
   {
     "url": "polyfill-ee1e201f602068867daf.js"
