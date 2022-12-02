7aead995ef5f26874a2b2acd7f9370d557a99464
Génesis León <genesis.leon.carbajal@gmail.com>
2022-12-01 19:48:28 -0500

    avance

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -24,29 +24,30 @@ export default function BlogPost({ data, location }) {
     }
     const { ogimage } = post.frontmatter
     const ogImagePath = ogimage && ogimage.childImageSharp.fixed.src
-    return ( < >
-        <    Helmet >
-        <script src = "https://d3js.org/d3.v6.min.js" > </script> 
-        <script src = "https://cdn.jsdelivr.net/npm/billboard.js/dist/billboard.min.js" > </script> 
-        <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/billboard.js/dist/billboard.min.css" />
-        <script src = "https://public.flourish.studio/resources/embed.js" > </script> 
-<script type="module" src="/graphs.js"></script> 
-</Helmet> 
-<SEO title = { data.markdownRemark.frontmatter.title }
+    return ( <>
+        <Helmet >
+        <script src = "https://d3js.org/d3.v6.min.js" > </script>  
+        <script src = "https://cdn.jsdelivr.net/npm/billboard.js/dist/billboard.min.js" > </script>  
+        <link rel = "stylesheet"
+        href = "https://cdn.jsdelivr.net/npm/billboard.js/dist/billboard.min.css" />
+        <script src = "https://public.flourish.studio/resources/embed.js" > </script>  
+        <script type = "module"
+        src = "/graphs.js" > </script>  
+        </Helmet>  <
+        SEO title = { data.markdownRemark.frontmatter.title }
         description = { data.markdownRemark.frontmatter.description }
         image = { ogImagePath }
         /> <HeaderBack />
         <Layout >
-        <HeadingXL > { data.markdownRemark.frontmatter.title } </HeadingXL>  
-<TextDate > { data.markdownRemark.frontmatter.date } </TextDate>  
-<TextBody dangerouslySetInnerHTML = {
+        <HeadingXL > { data.markdownRemark.frontmatter.title } </HeadingXL>   
+        <TextDate > { data.markdownRemark.frontmatter.date } </TextDate>   <
+        TextBody dangerouslySetInnerHTML = {
             { __html: data.markdownRemark.html }
         }
         /> <
         Disqus config = { disqusConfig }
-        />  
-</Layout > 
-</>
+        />   </Layout >  
+        </>
     )
 }
 
