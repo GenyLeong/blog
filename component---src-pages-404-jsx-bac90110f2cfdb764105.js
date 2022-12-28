2a0480bf250beb4d577d4f05092aa9878045434d
Génesis León <genesis.leon.carbajal@gmail.com>
2022-12-28 01:31:51 -0500

    Updates

---
+++ component---src-pages-404-jsx-bac90110f2cfdb764105.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[256],{7291:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var u=n(2549),r=n(7294),a=n(2416),c=n(7241);function l(){return r.createElement(a.Z,null,r.createElement(c.Z,{title:"Not found"}),r.createElement(u.E,{marginX:"auto",src:"https://cdni.iconscout.com/illustration/premium/thumb/page-not-found-2511615-2133703.png"}))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-bac90110f2cfdb764105.js.map
